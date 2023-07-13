886df555725787c9fa98dd1243cdca4ad550798e
Vaishali Maheshwari <vaishali.maheshwari4180@gmail.com>
2023-07-13 23:11:43 +0530

    make a mock data for users

--- js/join.test.js
+++ js/join.test.js
@@ -1,7 +1,14 @@
 const puppeteer = require('puppeteer');
+const {user} = require('../mock-data/users/index');
 const {correctData, incorrectData} = require('../mock-data/join/join');
 let browser, page;
+let BASE_URL = 'http://localhost:5500/join.html';
 describe('testing for the join page', () => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  };
   beforeAll(async () => {
     browser = await puppeteer.launch({
       headless: false,
@@ -9,7 +16,26 @@ describe('testing for the join page', () => {
     });
    
     const page = await browser.newPage();
-    await page.goto('https://dev.realdevsquad.com/join.html');
+    await page.goto('http://localhost:5500/join.html');
+
+    await page.setRequestInterception(true);
+
+    
+    page.on('request', (interceptedRequest) => {
+      const url = interceptedRequest.url();
+      console.log(url);
+      if (url === `${BASE_URL}`) {
+        interceptedRequest.respond({
+          status: 200,
+          contentType: 'application/json',
+          headers,
+          body: JSON.stringify({...user}),
+        });
+      }  else {
+        interceptedRequest.continue();
+      }
+    });
+
   });   
 
   it('testing for an input box area of introduction for correct data', async () => {
@@ -22,16 +48,7 @@ describe('testing for the join page', () => {
     expect(countWords(correctData)).toEqual(100);
   
   });
-  it('testing for an input box area of introduction for incorrect data', async () => {
-
-    let introductionSection = await page.$('#introduction');
-    const boundingBox = await introductionSection.boundingBox();
-    const x = boundingBox.x + boundingBox.width + 1; 
-    const y = boundingBox.y + boundingBox.height + 1;
-    await page.mouse.move(x, y);
-    expect(countWords(incorrectData)).toEqual(100);
   
-  });
 
  
 });
---
+++ mock-data/users/index.js
@@ -0,0 +1,20 @@
+const user = {
+   users:  [
+        {
+            first_name: "Vaishali",
+            github_display_name: "Vaishali Maheshwari",
+            github_id: "vaishalimaheshwari2000",
+            incompleteUserDetails: true,
+            last_name: "Maheshwari",
+            roles: {
+                archived: false,
+                in_discord: false
+
+            },
+            username: "vaishali"
+
+        }
+
+    ] 
+};
+module.exports = {user};
